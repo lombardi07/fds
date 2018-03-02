5927e619873d92bf563b77cd695a988c80144a8b
lombardi07 <36699963+lombardi07@users.noreply.github.com>
2018-03-02 15:44:29 +0100

    Update evac.f90

--- Source/evac.f90
+++ Source/evac.f90
@@ -15205,7 +15205,7 @@ CONTAINS
        ! Write the 'fed' columns
        IF (ii_density > ii_ntargets) THEN
           WRITE(tcform,'(a,i4.4,a,a,i4.4,a,a)') "(ES13.5E3,", n_cols, "(',',i8)", "," , &
-               ii_density-ii_ntargets, "(',',ES13.5E3)", ",',',i8,',',ES13.5E3,',',ES13.5E3,',',ES13.5E3,',',ES13.5E3)" ! Purser "fractional dose concept"
+               ii_density-ii_ntargets, "(',',ES13.5E3)", ",',',i8,',',ES13.5E3,',',ES13.5E3,',',ES13.5E3,',',ES13.5E3,',',ES13.5E3)" ! Purser "fractional dose concept"
           WRITE (LU_EVACCSV,fmt=tcform) Tin, n_tot_humans, &
                (MESHES(EVAC_Node_List(i)%IMESH)%N_HUMANS, i=1,n_egrids), &
                (EVAC_CORRS(i)%n_inside, i = 1,n_corrs), &
@@ -15213,17 +15213,17 @@ CONTAINS
                (EVAC_DOORS(i)%ICOUNT, i = 1,N_DOORS), &
                (NINT(ITEMP(i)), i = 1,N_EXITS-n_co_exits+N_DOORS), &
                (ITEMP(i), i = ii_ntargets+1,ii_density), &
-               n_dead, fed_max, fed_max_alive, ftd_max, ftd_max_alive, EXP_TMP ! Purser "fractional dose concept"
+               n_dead, fed_max, fed_max_alive, ftd_max, ftd_max_alive, exp_tmp ! Purser "fractional dose concept"
        ELSE
           WRITE(tcform,'(a,i4.4,a,a)') "(ES13.5E3,",n_cols+1, &
-               "(',',i8)", ",',',ES13.5E3,',',ES13.5E3,',',ES13.5E3,',',ES13.5E3)" ! Purser "fractional dose concept"
+               "(',',i8)", ",',',ES13.5E3,',',ES13.5E3,',',ES13.5E3,',',ES13.5E3,',',ES13.5E3)" ! Purser "fractional dose concept"
           WRITE (LU_EVACCSV,fmt=tcform) Tin, n_tot_humans, &
                (MESHES(EVAC_Node_List(i)%IMESH)%N_HUMANS, i=1,n_egrids), &
                (EVAC_CORRS(i)%n_inside, i = 1,n_corrs), &
                (EVAC_EXITS(i)%ICOUNT, i = 1,N_EXITS), &
                (EVAC_DOORS(i)%ICOUNT, i = 1,N_DOORS), &
                (NINT(ITEMP(i)), i = 1,N_EXITS-n_co_exits+N_DOORS), &
-               n_dead, fed_max, fed_max_alive, ftd_max, ftd_max_alive, EXP_TMP ! Purser "fractional dose concept"
+               n_dead, fed_max, fed_max_alive, ftd_max, ftd_max_alive, exp_tmp ! Purser "fractional dose concept"
        END IF
     ELSE
        ! Do not write the 'fed' columns
