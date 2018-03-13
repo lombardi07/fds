064a776e58289ca837a5df5805adabed874af1af
lombardi07 <36699963+lombardi07@users.noreply.github.com>
2018-03-13 22:48:12 +0100

    Update evac.f90

--- Source/evac.f90
+++ Source/evac.f90
@@ -15241,8 +15241,7 @@ CONTAINS
        ! Write the 'fed' columns
        IF (ii_density > ii_ntargets) THEN
           WRITE(tcform,'(a,i4.4,a,a,i4.4,a,a,a)') "(ES13.5E3,", n_cols, "(',',i8)", "," , & ! losa: text type "a" for new entries
-               ii_density-ii_ntargets, "(',',ES13.5E3)", ",',',i8,',',ES13.5E3,',',ES13.5E3,", & ! losa: new line
-               "2(',',ES13.5E3),8(',',i8))" ! losa: adding output columns to format
+               ii_density-ii_ntargets, "(',',ES13.5E3)", ",',',i8,',',4(',',ES13.5E3),1(',',i8),7(',',ES13.5E3))" ! losa: adding output columns to format
           WRITE (LU_EVACCSV,fmt=tcform) Tin, n_tot_humans, &
                (MESHES(EVAC_Node_List(i)%IMESH)%N_HUMANS, i=1,n_egrids), &
                (EVAC_CORRS(i)%n_inside, i = 1,n_corrs), &
@@ -15252,7 +15251,7 @@ CONTAINS
                (ITEMP(i), i = ii_ntargets+1,ii_density), &
                n_dead, fed_max, fed_max_alive, & ! losa: new line
                ftd_max,ftd_max_alive, & ! losa: purser's fractional thermal dose (FTD) concept
-               lim_n_dead,n_dead,n_dead,n_dead,n_dead,n_dead,n_dead,n_dead!lim_n_dead,lim_tmp,lim_rad,lim_co,lim_co2,lim_o2,lim_hcn,lim_vis ! losa: incapacitation limits
+               lim_n_dead,lim_tmp,lim_rad,lim_co,lim_co2,lim_o2,lim_hcn,lim_vis ! losa: incapacitation limits
        ELSE
           WRITE(tcform,'(a,i4.4,a,a)') "(ES13.5E3,",n_cols+1, & ! losa: text type "a" for new entries
                "(',',i8)", ",',',4(',',ES13.5E3),1(',',i8),7(',',ES13.5E3))" ! losa: adding output columns to format
@@ -15264,7 +15263,7 @@ CONTAINS
                (NINT(ITEMP(i)), i = 1,N_EXITS-n_co_exits+N_DOORS), &
                n_dead, fed_max, fed_max_alive, & ! losa: new line
                ftd_max,ftd_max_alive, & ! losa: purser's fractional thermal dose (FTD) concept
-               lim_n_dead,n_dead,n_dead,n_dead,n_dead,n_dead,n_dead,n_dead!lim_n_dead,lim_tmp,lim_rad,lim_co,lim_co2,lim_o2,lim_hcn,lim_vis ! losa: incapacitation limits
+               lim_n_dead,lim_tmp,lim_rad,lim_co,lim_co2,lim_o2,lim_hcn,lim_vis ! losa: incapacitation limits
        END IF
     ELSE
        ! Do not write the 'fed' columns
