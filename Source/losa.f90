3a128f1cf8bac75536e22b8b319e7b0aad4206b0
lombardi07 <julian.maier@lombardi.group>
2018-03-20 18:40:18 +0100

    joules: testing git from windows

--- Source/losa.f90
+++ Source/losa.f90
@@ -50,4 +50,6 @@
 ! the fire calculation through the function "GET_FIRE_CONDITIONS". data of the "HUMAN_GRID" is
 ! saved on the disk, while concentrations from "EVAC_CORRS" are dumped.
 ! outputs are written in the *evac.csv file.
-!ciao
+
+
+! im adding a new line here to test
